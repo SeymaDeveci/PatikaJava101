247f34574f1f9a8d9805f7e7bb11476c7991b9d6
SeymaDeveci <seymadeveci@hotmail.com>
2022-03-15 20:36:58 +0300

    Java 101

--- ManavKasaProgrami.java
+++ ManavKasaProgrami.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-
 public class ManavKasaProgrami {
     public static void main(String[] args) {
-        double kilo_ar = 2.14, kilo_el = 3.67, kilo_dom = 1.11,
-                kilo_muz = 0.95, kilo_pat = 5.00, total;
+        double armut = 2.14, elma = 3.67, domates = 1.11,
+                muz = 0.95, patlican = 5.00, total;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Armut Kaç Kilo ? : ");
-        double armut = input.nextDouble();
+        armut *= input.nextDouble();
 
         System.out.print("Elma Kaç Kilo ? : ");
-        double elma = input.nextDouble();
+        elma *= input.nextDouble();
 
         System.out.print("Domates Kaç Kilo ? : ");
-        double domates = input.nextDouble();
+        domates *= input.nextDouble();
 
         System.out.print("Muz Kaç Kilo ? : ");
-        double muz = input.nextDouble();
+        muz *= input.nextDouble();
 
         System.out.print("Patlıcan Kaç Kilo ? : ");
-        double patlican = input.nextDouble();
+        patlican *= input.nextDouble();
 
-        total = (kilo_ar * armut) + (kilo_el * elma) + (kilo_dom * domates)
-                + (kilo_muz * muz) + (kilo_pat * patlican);
+        total = armut + elma + domates + muz + patlican;
         System.out.print("Toplam Tutar : " + total);
     }
 }
