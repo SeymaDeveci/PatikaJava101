0f39aaa12e01446ac36e488f13291d44bea62b7c
SeymaDeveci <seymadeveci@hotmail.com>
2022-03-26 12:51:46 +0300

    Java 101 Math Sınıfı Static Fonksiyonları

---
+++ MathSinifi.java
@@ -0,0 +1,19 @@
+public class MathSinifi {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-100)); //Mutlak değer
+        System.out.println(Math.cbrt(125)); //Küp kökünü verir
+        System.out.println(Math.ceil(2.3)); //En yakın bir üst tam sayıyı verir
+        System.out.println(Math.floor(1.99999)); //En yakın birt alt tam sayıyı verir
+        System.out.println(Math.log(100)); //Logaritmayı e tabanında verir
+        System.out.println(Math.log10(100)); //Logaritmayı 10 tabanında verir
+        System.out.println(Math.max(100,1000)); //En büyük sayıyı verir
+        System.out.println(Math.min(100,1000)); //En küçük sayıyı verir
+        System.out.println(Math.pow(2,3)); //Üs alma
+        System.out.println(Math.random() * 10); //0 ile 1 arasında rastgele bir sayıyı verir
+        System.out.println(Math.round(10.49)); //En yakın sayıya yuvarlar int olarak
+        System.out.println(Math.signum(-1000)); //İşaretini verir negatif ise -1.o döndürür.
+        System.out.println(Math.sqrt(16)); //Karekökünü verir
+        System.out.println(Math.tan(60)); //Tanjantını verir
+        
+    }
+}
